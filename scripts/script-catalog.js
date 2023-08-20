69240636be821a0f815c0213f1632f9e517d5eb2
danskor <skorokhodd@yandex.ru>
2023-08-20 22:07:26 +0300

    add slider on JS

--- scripts/script-catalog.js
+++ scripts/script-catalog.js
@@ -1,6 +1,7 @@
-let menuButton = document.querySelector('.menu-item-button');
+let menuButton = document.querySelector('.menu-item-catalog-link');
 let itemCatalog = document.querySelector('.nav-menu-item-catalog');
 
-menuButton.onclick = function() {
+menuButton.onclick = function(evt) {
+  evt.preventDefault();
   itemCatalog.classList.toggle('has-submenu');
 };
